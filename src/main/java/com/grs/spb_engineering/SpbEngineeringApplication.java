58b6cfd9866e34c50c13d89f081a7bb6863f95fb
golam_rabbany <golamrabbanyshikder@gmail.com>
2024-02-12 11:27:45 +0600

    First commit

---
+++ src/main/java/com/grs/spb_engineering/SpbEngineeringApplication.java
@@ -0,0 +1,13 @@
+package com.grs.spb_engineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbEngineeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbEngineeringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grs/spb_engineering/SpbEngineeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grs.spb_engineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbEngineeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
